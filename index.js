e245d685218a30eb90ecf8513bb4944b0cbc6724
amin jamal <aminjamal10@gmail.com>
2018-04-06 10:14:40 -0700

    dd

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const Telegraf = require('telegraf')
 
 const bot = new Telegraf("577508932:AAEWz4n7dJzYJ9ooHCZarXmjWoeh9ONBp7c")
 
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-bot.hears(/buy/i, (ctx) => ctx.reply('Buy-buy'))
-bot.hears("get list",(ctx)=>{
-    ctx.reply("list")
 
-})
+// bot.help((ctx) => ctx.reply('Send me a sticker'))
+// bot.on('sticker', (ctx) => ctx.reply('👍'))
+// bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+// bot.hears(/buy/i, (ctx) => ctx.reply('Buy-buy'))
+// bot.hears("get list",(ctx)=>{
+//     ctx.reply("list")
+
+// })
 const testMenu = Telegraf.Extra
   .markdown()
   .markup((m) => m.inlineKeyboard([
@@ -22,7 +22,7 @@ const aboutMenu = Telegraf.Extra
   .markup((m) => m.keyboard([
     m.callbackButton('⬅️ Back')
   ]).resize())
-
+bot.start((ctx) => ctx.reply('Welcome'))
 bot.hears('test', (ctx) => {
   ctx.reply('test message', testMenu).then(() => {
     ctx.reply('about', aboutMenu)
